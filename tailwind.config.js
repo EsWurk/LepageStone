0a73be055e04f0c6979e24716b2f58cfe8929720
EsWurk <jorisgerardh@gmail.com>
2023-05-19 20:43:45 +0200

    Ajout de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
 }
-
